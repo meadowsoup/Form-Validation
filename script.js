884393bccf412d8bb14be44bf1a7207a8a50f2d3
adelavega <136393880+meadowsoup@users.noreply.github.com>
2025-02-07 19:50:04 -0500

    fields

--- script.js
+++ script.js
@@ -20,116 +20,169 @@ function hideError() {
      errorDisplay.style.display = "none";
 }
 
-//* registration form validation
-form.addEventListener('submit', function(e) {
-     e.preventDefault();
-});
-
 //* focus on input field with error
 function focusOnError(inputEle) {
      inputEle.focus();
 }
 
-//* reset errors
-hideError();
+//* registration form validation
+form.addEventListener('submit', function(e) {
+     e.preventDefault();
 
-//* validate user
-if (username.value.trim() === "") {
-     showError("Username no blank!");
-     focusOnError(username);
-     return;
-}
-if (username.value.length < 4) {
-     showError("Username has got to AT LEAST be 4 chars long bruh 🙄");
-     focusOnError(username);
-     return;
-}
-if (!/^[A-Za-z0-9]+$/.test(username.value)) {
-     showError("No special chars or whitespaces in username dummy 🤦🏽‍♂️!");
-     focusOnError(username);
-     return;
-}
-if (new Set(username.value).size < 2) {
-     showError("At least 2 unique chars in username 🙏🏽");
-     focusOnError(username);
-     return;
-}
+     //* reset errors
+     hideError();
+     
+     //* validate user
+     if (username.value.trim() === "") {
+          showError("Username no blank!");
+          focusOnError(username);
+          return;
+     }
+     if (username.value.length < 4) {
+          showError("Username has got to AT LEAST be 4 chars long bruh 🙄");
+          focusOnError(username);
+          return;
+     }
+     if (!/^[A-Za-z0-9]+$/.test(username.value)) {
+          showError("No special chars or whitespaces in username dummy 🤦🏽‍♂️!");
+          focusOnError(username);
+          return;
+     }
+     if (new Set(username.value).size < 2) {
+          showError("At least 2 unique chars in username 🙏🏽");
+          focusOnError(username);
+          return;
+     }
+     
+     //* email validation
+     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
+          showError("Valid email plz! 😤");
+          return;
+     }
+     if (email.value.toLowerCase().endsWith("@ex.com")) {
+          showError("Email can't be from 'ex.com' domain btw...");
+          focusOnError(email);
+          return;
+     }
+     
+     //* validating password
+     if (password.value.length < 12) {
+          showError("Password must be at least 12 characters long.");
+          focusOnError(password);
+          return;
+     }
+     if (!/[A-Z]/.test(password.value) || !/[a-z]/.test(password.value)) {
+          showError("At least one uppercase and lowercase letter 😐");
+          focusOnError(password);
+          return;
+     }
+     if (!/\d/.test(password.value)) {
+          showError("at least 1 #.");
+          focusOnError(password);
+          return;
+     }
+     if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password.value)) {
+          showError("password must contain at least 1 special character.");
+          focusOnError(password);
+          return;
+     }
+     if (/password/i.test(password.value)) {
+          showError("cannot contain 'password' 😑...");
+          focusOnError(password);
+          return;
+     }
+     if (password.value.toLowerCase().includes(username.value.toLowerCase())) {
+          showError("cannot contain the username 👹");
+          focusOnError(password);
+          return;
+     }
+     if (password.value !== passwordCheck.value) {
+          showError("Doesn't match!");
+          focusOnError(password);
+          return;
+     }
+     
+     //* validate terms and conditions
+     if (!terms.checked) {
+          showError("Agree to the terms 😈");
+          focusOnError(terms);
+          return;
+     }
+     
+     //* Check if username is taken
+     const users = JSON.parse(localStorage.getItem('users')) || [];
+     const usernameExists = users.some(user => user.username.toLowerCase() === username.value.toLowerCase());
+     
+     if (usernameExists) {
+          showError("Username already taken 🤡");
+          focusOnError(username);
+          return;
+     }
+     
+     //* store user data in local storage
+     const user = {
+          username: username.value.toLowerCase(),
+          email: email.value.toLowerCase(),
+          password: password.value
+     };
+     
+     users.push(user);
+     localStorage.setItem('users', JSON.stringify(users));
+     
+     //* clear form fields and show success message
+     form.reset();
+     hideError();
+     alert("Registration Successful! 🎊")
+});
 
-//* email validation
-if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
-     showError("Valid email plz! 😤");
-     return;
-}
-if (email.value.toLowerCase().endsWith("@ex.com")) {
-     showError("Email can't be from 'ex.com' domain btw...");
-     focusOnError(email);
-     return;
-}
+//* login form validation
+const loginForm = document.getElementById('login');
+const loginUsername = loginForm.elements.username;
+const loginPassword = loginForm.elements.password;
+const persist = loginForm.elements.persist;
 
-//* validating password
-if (password.value.length < 12) {
-     showError("Password must be at least 12 characters long.");
-     focusOnError(password);
-     return;
-}
-if (!/[A-Z]/.test(password.value) || !/[a-z]/.test(password.value)) {
-     showError("At least one uppercase and lowercase letter 😐");
-     focusOnError(password);
-     return;
-}
-if (!/\d/.test(password.value)) {
-     showError("at least 1 #.");
-     focusOnError(password);
-     return;
-}
-if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password.value)) {
-     showError("password must contain at least 1 special character.");
-     focusOnError(password);
-     return;
-}
-if (/password/i.test(password.value)) {
-     showError("cannot contain 'password' 😑...");
-     focusOnError(password);
-     return;
-}
-if (password.value.toLowerCase().includes(username.value.toLowerCase())) {
-     showError("cannot contain the username 👹");
-     focusOnError(password);
-     return;
-}
-if (password.value !== passwordCheck.value) {
-     showError("Doesn't match!");
-     focusOnError(password);
-     return;
-}
+loginForm.addEventListener('submit', function(e) {
+     e.preventDefault(); // prevent form submission
 
-//* validate terms and conditions
-if (!terms.checked) {
-     showError("Agree to the terms 😈");
-     focusOnError(terms);
-     return;
-}
+     hideError();
 
-//* Check if username is taken
-const users = JSON.parse(localStorage.getItem('users')) || [];
-const usernameExists = users.some(user => user.username.toLowerCase() === username.value.toLowerCase());
+     //* validate username
+     if (loginUsername.value.trim() === "") {
+          showError("Username can't be 🚫");
+          focusOnError(loginUsername);
+          return;
+     }
 
-if (usernameExists) {
-     showError("Username already taken 🤡");
-     focusOnError(username);
-     return;
-}
+     //* validate password
+     if (loginPassword.value.trim() === "") {
+          showError("Password can't be 🚫");
+          focusOnError(loginPassword);
+          return;
+     }
 
-//* store user data in local storage
-const user = {
-     username: username.value.toLowerCase(),
-     email: email.value.toLowerCase(),
-     password: password.value
-};
-users.push(user);
-localStorage.setItem('users', JSON.stringify(users));
+     //* checking if username exists
+     const users = JSON.parse(localStorage.getItem('users')) || [];
+     const user = users.find(user => user.username === loginUsername.value.toLowerCase());
 
-//* clear form fields and show success message
-form.reset();
-hideError();
-alert("Registration Successful! 🎊")
+     if (!user) {
+          showError("Username does not exist");
+          focusOnError(loginUsername);
+          return;
+     }
+
+     // validating password
+     if (user.password !== loginPassword.value) {
+          showError("Incorrect password sir🤚🏽");
+          focusOnError(loginPassword);
+          return;
+     }
+
+     // clear form fields and show success message again
+     loginForm.reset();
+     hideError();
+     if (persist.checked) {
+          alert("Success. Staying logged in")
+     } else {
+          alert("Login successful!");
+     }
+});
